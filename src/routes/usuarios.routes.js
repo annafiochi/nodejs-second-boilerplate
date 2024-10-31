b6fe59e910f5f87fb960116850113b0f72b3c7b1
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-10-31 11:20:59 -0300

    feat: create update user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -5,49 +5,62 @@ const usuariosRoutes = Router();
 
 const usersRepository = new UsersRepository();
 
-usuariosRoutes.get("/", (req,res) => { 
-    const usuarios = usersRepository.getAllUsers();
+usuariosRoutes.get("/", (req, res) => {
+  const usuarios = usersRepository.getAllUsers();
 
-    return res.status(200).json({
-        message:
-        usuarios.length == 0
+  return res.status(200).json({
+    message:
+      usuarios.length == 0
         ? "Não há usuários cadastrados"
         : `Total de usuários: ${usuarios.length}`,
-        usuarios,
-    });
+    usuarios,
+  });
 });
 
-usuariosRoutes.post("/", (req,res) => {
-    const {name, email, password} = req.body;
+usuariosRoutes.post("/", (req, res) => {
+  const { name, email, password } = req.body;
 
-    const usuario = usersRepository.addUser(name, email, password);
+  const usuario = usersRepository.addUser(name, email, password);
 
-    return res.status(201).json({
-        message: "Usuário cadastrado com sucesso",
-        usuario,
-    });
-    
+  return res.status(201).json({
+    message: "Usuário cadastrado com sucesso",
+    usuario,
+  });
 });
 
-usuariosRoutes.get("/:id", (req,res) => {
-    const {id} = req.params;
-   const user = usersRepository.getUserByld(id);
+usuariosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const user = usersRepository.getUserByld(id);
 
-   if (!user) {
+  if (!user) {
     return res.status(404).json({
-        message: `usuario com id ${id} não encontrado`,
-     });
-   }
+      message: `usuario com id ${id} não encontrado`,
+    });
+  }
 
-   return res.status(200).json({
+  return res.status(200).json({
     message: `usuario com id ${id}encontrado`,
-     });
+  });
 });
-usuariosRoutes.put("/:id", (req,res) => {
 
-})
-usuariosRoutes.delete("/:id", (req,res) => {
+usuariosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
 
-})
+  const user = usersRepository.updateUser(id, name, email, password);
+
+  if (!user) {
+    return res.status(404).json({
+      message: `usuario com id ${id} não encontrado`,
+    });
+  }
+
+  return res.status(200).json({
+    message: `usuario com id ${id} atualizado com sucesso`,
+    user,
+  });
+});
+
+usuariosRoutes.delete("/:id", (req, res) => {});
 
 export default usuariosRoutes;
