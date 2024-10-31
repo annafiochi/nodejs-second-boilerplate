c79871f84d1b76989a60896c5cf23147f129304f
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-10-31 08:49:14 -0300

    create users class

---
+++ src/models/users/User.js
@@ -0,0 +1,12 @@
+ class User {
+    constructor(name, email, password){
+        this.id = this.generalteId();
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+
+    generalteId() {
+        return Math.floor(math.random() * 999) +1;
+    }
+ }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const usuariosRoutes = Router();
 
 let usuarios = [];
+
 usuariosRoutes.get("/", (req,res) => {
     return res.status(200).json({
         message:
